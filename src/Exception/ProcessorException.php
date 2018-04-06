630e196d686152e4a02697691e3ed02fc569d543
Ahmed TAILOULOUTE <ahmed.tailouloute@gmail.com>
2018-04-06 12:04:46 +0200

    Update ProcessorException.php

--- src/Exception/ProcessorException.php
+++ src/Exception/ProcessorException.php
@@ -2,6 +2,21 @@
 
 namespace AT\CodeQualityTool\Exception;
 
+use Throwable;
+
 class ProcessorException extends \Exception
 {
+    private $warning;
+
+    public function __construct(string $message = "", bool $warning = false, int $code = 0, Throwable $previous = null)
+    {
+        $this->warning = $warning;
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function isWarning(): bool
+    {
+        return $this->warning;
+    }
 }
